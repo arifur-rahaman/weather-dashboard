9c4e84061488eb7eea2349508f1997ebae3ec74f
arifur-rahaman <arif.rahman041@gmail.com>
2024-11-02 23:43:58 +0600

    feat: add structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/header/weather/WeatherBoard";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 }
