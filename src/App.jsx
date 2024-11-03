22fed7bdd69fc5c1bd528a4bb63dac05173b77bb
arifur-rahaman <arif.rahman041@gmail.com>
2024-11-03 21:38:18 +0600

    feat: add useWeather custom hook and openweather api

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import "./App.css";
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/header/weather/WeatherBoard";
 
+// api key: f0bbd31e7c47cf24fed175d28afe4139
+
 function App() {
   return (
-    <>
+    <div className="grid place-items-center h-screen">
       <Header />
       <main>
         <section>
           <WeatherBoard />
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
