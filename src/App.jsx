a83426d49ca0948d99e46f76c8e03fb156bff21b
arifur-rahaman <arif.rahman041@gmail.com>
2024-11-04 17:09:06 +0600

    feat: add weather data from api to UI as per current location of browswer

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import "./App.css";
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/header/weather/WeatherBoard";
 
-// api key: f0bbd31e7c47cf24fed175d28afe4139
+import { WeatherProvider } from "./provider";
 
 function App() {
   return (
-    <div className="grid place-items-center h-screen">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <WeatherProvider>
+      <div className="grid place-items-center h-screen">
+        <Header />
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
+    </WeatherProvider>
   );
 }
 
--- src/components/header/weather/WeatherHeadline.jsx
+++ src/components/header/weather/WeatherHeadline.jsx
@@ -1,22 +1,63 @@
+import { useContext } from "react";
 import Cloud from "../../../assets/cloud.svg";
+import Haze from "../../../assets/haze.svg";
+import Snow from "../../../assets/icons/snow.svg";
+import Sunny from "../../../assets/icons/sunny.svg";
 import Pin from "../../../assets/pin.svg";
+import Rain from "../../../assets/rainy.svg";
+import Thunder from "../../../assets/thunder.svg";
+import { WeatherContext } from "../../../context";
+import { getFormattedDate } from "../../../utils/date-util";
 
 export default function WeatherHeadline() {
+  const { weatherData } = useContext(WeatherContext);
+
+  const { climate, location, temperature, time } = weatherData;
+
+  function getWeatherIcon(climate) {
+    switch (climate) {
+      case "Rain":
+        return Rain;
+      case "Clouds":
+        return Cloud;
+      case "Snow":
+        return Snow;
+      case "Rain":
+        return Rain;
+      case "Thunder":
+        return Thunder;
+      case "Clear":
+        return Sunny;
+      case "Haze":
+        return Haze;
+      case "Fog":
+        return Haze;
+      case "Mist":
+        return Haze;
+
+      default:
+        return Sunny;
+    }
+  }
+
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src={Cloud} alt="cloud" />
+        <img src={getWeatherIcon(climate)} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(temperature)}
           </h1>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={Pin} />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
         </div>
       </div>
-      <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+      <p className="text-sm lg:text-lg">
+        {getFormattedDate(time, "time", false)} -{" "}
+        {getFormattedDate(time, "date", false)}
+      </p>
     </div>
   );
 }
